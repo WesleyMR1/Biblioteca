7c85f481fa1ec50780a49f52fb63ad8adb3a8dc8
WesleyMR1 <WesleyMR012@gmail.com>
2024-01-12 20:21:19 -0300

    initial 1

---
+++ View/navbar.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="../View/links/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand bg-dark navbar-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <span class="navbar-brand">
+                    Biblioteca
+                </span>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Livros</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Clientes</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Locação</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <script src="../View/links/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php    
+<?php
+include('./View/navbar.php');
+?>
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="View/links/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="Container">
+        <div class="row">
+            <div class="col">
+                <h1>Hello, world!</h1>
+            </div>
+        </div>
+    </div>
+
+    <script src="../View/links/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
